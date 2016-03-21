f0789414a063b9a05c3b89928a9e9506742a3e10
thr0w <JoseFernandoTolentino@gmail.com>
2016-03-21 09:45:31 -0300

    teste de capacidade no websql

--- appwebsql.js
+++ appwebsql.js
@@ -1,17 +1,53 @@
 if (Meteor.isClient) {
     var select, db;
     var cont = 0;
+    var _cont_muitos = new ReactiveVar();
     Template.formTest.onRendered(function () {
         select = document.getElementById('select');
         setupDatabase();
     });
 
+    Template.formTest.helpers({
+        cont: function () {
+            return _cont_muitos.get()
+        }
+    });
     Template.formTest.events({
         'click #incluir': function () {
             save();
+        },
+        'click #muitos': function () {
+            debugger
+            if (document.getElementById('muitos').checked)
+                muitos();
         }
     });
 
+
+    function muitos() {
+        debugger
+        var mais = document.getElementById('muitos');
+        var k = [];
+        for (var i = 1; i <= 1024; i++)
+            k.push('-');
+        k = k.join('');
+        var m = [];
+        for (var i = 1; i <= 1024; i++)
+            m.push(k);
+        m = m.join('');
+        function inclui() {
+            _cont_muitos.set(_cont_muitos.get() + 1);
+            db.transaction(function (tx) {
+                tx.executeSql('INSERT INTO testeDB VALUES (?, ?)', [_cont_muitos.get(), m], function (tx, res) {
+                    console.log(res)
+                    if (res.rowsAffected != 1) alert('erro no insert')
+                    if (mais.checked)
+                        setTimeout(inclui, 1);
+                });
+            });
+        }
+        setTimeout(inclui, 1);
+    }
 
     function save() {
         var texto = document.getElementById('texto');
@@ -44,10 +80,14 @@ if (Meteor.isClient) {
             select.innerHTML = '<li>Web SQL Database API is not available in this browser, please try nightly Opera, Webkit or Chrome.</li>';
             return;
         }
-        db = openDatabase('testeDB', '1.0', 'banco de teste', 30 * 1024 * 1024);
+        db = openDatabase('testeDB', '1.0', 'banco de teste', 30 * 1024 * 1024 * 1024);
         db.transaction(function (tx) {
-            //tx.executeSql('DROP TABLE IF EXISTS testeDB');
-            //tx.executeSql('CREATE TABLE IF NOT EXISTS testeDB (id unique, text)');
+            debugger
+            // tx.executeSql('DROP TABLE IF EXISTS testeDB');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS testeDB (id unique, text)');
+            tx.executeSql('select count(*) as cont from testeDB', [], function (tx, res) {
+                _cont_muitos.set(res.rows[0].cont);
+            });
         });
     }
 }
